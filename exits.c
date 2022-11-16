ca3adb554ec207fc359d84a9d4cc685b35500770
Adepoju Usman <adepojuusman465@gmail.com>
2022-11-16 02:13:52 +0000

    locates a character in a string

--- exits.c
+++ exits.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  **_strncpy - copies a string
@@ -57,3 +57,18 @@ dest[i] = '\0';
 return (s);
 }
 
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+do {
+if (*s == c)
+return (s);
+} while (*s++ != '\0');
+
+return (NULL);
+}
