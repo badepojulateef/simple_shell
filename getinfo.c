905f34a63cbe4bda29f6d16b39e5dbbec5248d49
Adepoju Usman <adepojuusman465@gmail.com>
2022-11-16 05:30:45 +0000

    initializes info_t struct

---
+++ getinfo.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * clear_info - initializes info_t struct
+ * @info: struct address
+ */
+void clear_info(info_t *info)
+{
+info->arg = NULL;
+info->argv = NULL;
+info->path = NULL;
+info->argc = 0;
+}
+
