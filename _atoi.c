35df1e87d838a29c6f76af861fb04ab2fb7b34e6
Adepoju Usman <adepojuusman465@gmail.com>
2022-11-16 00:31:53 +0000

    checks for alphabetic character

--- _atoi.c
+++ _atoi.c
@@ -24,3 +24,17 @@ int is_delim(char c, char *delim)
 		return (1);
 	return (0);
 }
+
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
