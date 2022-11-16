bf0bb6de0e7c9fa84c61e5033b410c6e1a72426d
Adepoju Usman <adepojuusman465@gmail.com>
2022-11-16 01:30:44 +0000

    Initialize a new environment variable or modify an existing one

--- environ.c
+++ environ.c
@@ -34,4 +34,23 @@ node = node->next;
 return (NULL);
 }
 
+/**
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+if (info->argc != 3)
+{
+_eputs("Incorrect number of arguments\n");
+return (1);
+}
+if (_setenv(info, info->argv[1], info->argv[2]))
+return (0);
+return (1);
+}
+
 
