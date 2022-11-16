98a8530b94ae6e40a62819f9a38f0135e83b6817
Adepoju Usman <adepojuusman465@gmail.com>
2022-11-16 05:42:13 +0000

    reads a buffer

--- getline.c
+++ getline.c
@@ -89,4 +89,22 @@ return (_strlen(p)); /* return length of current command */
 return (r); /* return length of buffer from _getline() */
 }
 
+/**
+ * read_buf - reads a buffer
+ * @info: parameter struct
+ * @buf: buffer
+ * @i: size
+ *
+ * Return: r
+ */
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+ssize_t r = 0;
 
+if (*i)
+return (0);
+r = read(info->readfd, buf, READ_BUF_SIZE);
+if (r >= 0)
+*i = r;
+return (r);
+}
