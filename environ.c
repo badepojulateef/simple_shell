227ecb8e59b6899c429e31eada6d28fd024aceaa
Adepoju Usman <adepojuusman465@gmail.com>
2022-11-16 01:26:52 +0000

    prints the current environment

---
+++ environ.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
+{
+print_list_str(info->env);
+return (0);
+}
+
+
