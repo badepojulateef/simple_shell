36c928197c741e2570486eb2d341115c21cc8e26
Adepoju Lateef <badepojulateef@gmail.com>
2022-11-16 11:08:07 +0100

    added _strcat function

--- string1.c
+++ string1.c
@@ -58,3 +58,21 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
