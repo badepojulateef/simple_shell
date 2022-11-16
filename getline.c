4f44fbaaac66d6e124f82239d45b09ab01c3e566
Adepoju Usman <adepojuusman465@gmail.com>
2022-11-16 05:47:43 +0000

    blocks ctrl-C

--- getline.c
+++ getline.c
@@ -156,3 +156,15 @@ if (length)
 return (s);
 }
 
+/**
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+_puts("\n");
+_puts("$ ");
+_putchar(BUF_FLUSH);
+}
